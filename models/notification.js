5a8106d8cf35c3f7998eb311f122df16376e2a9c
Jonathan Rivas <jonathan@smartstart.us>
2021-03-30 19:19:55 +0800

    update 1 seconds ago

--- models/notification.js
+++ models/notification.js
@@ -6,10 +6,12 @@ const Schema = mongoose.Schema;
 const Notification = new Schema(
   {
     receiver: { type: Schema.Types.ObjectId, required: true, ref: "Account" },
-    initiator: {type: Schema.Types.ObjectId, required: false, ref: "Account"},
-    page: { type:  Schema.Types.ObjectId, ref: "Page"},
-    message: {type: String, required: [true, "Message is required"]},
-    openned: {type: Boolean, required: false, default: false}
+    initiator: { type: Schema.Types.ObjectId, required: false, ref: "Account" },
+    page: { type: Schema.Types.ObjectId, required: true, ref: "Page" },
+    booking: { type: Schema.Types.ObjectId, required: true, ref: "Booking" },
+    type: { type: String, enum: ["page", "page-message", "page-booking", "booking", "booking-message"], required: true },
+    message: { type: String, required: [true, "Message is required"] },
+    openned: { type: Boolean, required: false, default: false }
   },
   { timestamps: true }
 );
