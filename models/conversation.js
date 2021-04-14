fdf7b6be84e837fa3c26b3b98beb4c7aa9c1776f
Jonathan Rivas <jonathan@smartstart.us>
2021-04-15 01:48:39 +0800

    update

--- models/conversation.js
+++ models/conversation.js
@@ -8,7 +8,7 @@ const Conversation = new Schema(
     page: { type: Schema.Types.ObjectId, required: true, ref: "Page" },
     booking: { type: Schema.Types.ObjectId, required: true, ref: "Booking" },
     messages: [message],
-    receiver: {type: String, required: true, enum: ['owner', 'admin']},
+    receiver: { type: Schema.Types.ObjectId, required: true, ref: "Account" },
     opened: { type: Boolean, required: false, default: false }
   },
   { timestamps: true }
